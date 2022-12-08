1eb81848e4ffd6e3a836cb17fb586676376b6111
TheSlapLog <73797186+TheSlapLog@users.noreply.github.com>
2022-12-08 10:49:23 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const h1One = document.getElementById('h1One')
 const dateShow = document.getElementById('date-show') // Date show H3
 const buttonOne = document.getElementById('buttonOne')
 const body = document.getElementById('body')
+const h4One = document.getElementById('h4One')
 
 const currentDate = new Date()
 
@@ -11,5 +12,5 @@ function onClick() {
     body.classList.toggle('backgroundBlack')
     h1One.classList.toggle('colorWhite')
     dateShow.classList.toggle('colorWhite')
-
+    h4One.classList.toggle('colorWhite')
 }
